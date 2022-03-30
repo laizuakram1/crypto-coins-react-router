2056f3eb96e28cc877a9a89d349619628f2bd924
laizuakram1 <laizuakram98@gmail.com>
2022-03-30 23:39:33 +0600

    inital commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2 className='text-5xl text-purple-500'>this is header</h2>
+        </div>
+    );
+};
+
+export default Header;
